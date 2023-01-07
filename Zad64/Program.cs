b0eabc89b73acf4723288d413b4a3be3ba1867df
Dmitriy <dmitriyisaev2217736564@gmail.com>
2023-01-07 15:33:13 +0300

    Задача№64

---
+++ Zad64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1
+
+int n = InputInt("Задайте  положительное целое число:  ");
+int k = 1;
+if (n < 1)
+{
+    Console.WriteLine("Задано отрицательное число");
+}
+Console.WriteLine(Natural(n, k));
+
+int Natural(int n, int k)
+{
+    if (n == k)
+        return n;
+    else
+        Console.Write($"{Natural(n, k + 1)}, ");
+    return k;
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
